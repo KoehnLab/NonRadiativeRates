a2b519e2978a1c4014ad5b77e2de902afc9a3878
robert-toews <138822364+robert-toews@users.noreply.github.com>
2026-09-24 14:56:29 +0200

    Update IC_rate_constant.py
    Updated information

--- examples/IC_rate_constant.py
+++ examples/IC_rate_constant.py
@@ -8,31 +8,13 @@ from nonradiative_rates import read_turbomole as rtm
 from nonradiative_rates.constants import au2rcm, amu
 
 # Specify directories:
+# The data can be found on the data repository of the University of Stuttgart (DaRUS): doi:10.18419/DARUS-6477
 hessian_dir = "./relaxed-gradient_model/TTM-1Cz_GRAD-HESSIAN/"
 grad_dir = "./relaxed-gradient_model/TTM-1Cz_GRAD-HESSIAN/"
 nac_dir = "./relaxed-gradient_model/TTM-1Cz_D1-EGRAD/"
 
 # Specify energy gap of the non-radiative transition
 ad_exc = 1.5924e+4
-
-#def run_simulation(
-#        hessian_calculation=hessian_dir,
-#        egrad_calculation=grad_dir,
-#        i_state_gradient = False,   # gradient refers to initial state? T/F
-#        fcwd_file="FCWD.dat",       # output file for FCWD
-#        D = 30000,                  # diss. energy (for estimate of anharm.)
-#        anh_thr = 400.,             # use anh. approx for modes larger than this
-#        e_trans = ad_exc,         # energy where FCWD is measured
-#        sigma = 100.,               # width of Gaussian energy window centered at e_trans
-#        damp = 10.,                 # damping for low-frequency modes
-#        damp_thr = 100.,            # threshold for low-frequency modes
-#        low_freq_approx = 300.,     # classical approx. for modes < this val.
-#        thrmod = 1e-24,             # cutoff threshold for modes
-#        maxquanta = 200,            # max. quanta (cutoff should lead to smaller value)
-#        max_e = 25000.,             # maximum energy for comp. FCWD (e_trans + several sigma)
-#        e_bin = 1.,                 # binning for FCWD
-#        run_mode_tests = False       # see end of this routine
-#        ):
 
 
 def main():
